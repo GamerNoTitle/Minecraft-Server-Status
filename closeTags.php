9cc32bc01657c55c2e2b5ee77e572c3e21d472de
GoneTone <p29022716@gmail.com>
2018-08-12 17:29:19 +0800

    Bug fix

--- closeTags.php
+++ closeTags.php
@@ -1,9 +1,9 @@
 <?php
 function closeTags($html) {
-    preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
+    @preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
     $openedtags = $result[1];
 
-    preg_match_all('#</([a-z]+)>#iU', $html, $result);
+    @preg_match_all('#</([a-z]+)>#iU', $html, $result);
     $closedtags = $result[1];
 
     $len_opened = count($openedtags);
@@ -19,7 +19,7 @@ function closeTags($html) {
             unset($closedtags[array_search($openedtags[$i], $closedtags)]);
         }
     }
-    
+
     return $html;
 }
 ?>
