05025b4df501eaaf9405690acc3c5208c6b49dda
Pesy Wu <admin@bili33.top>
2021-06-07 18:11:42 +0800

    增加更多变量设置来提供更方便的修改模式

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 // 原作者为MCNewsTools，由GamerNoTitle二次修改，模板套用的HTML5UP的HTML模板，请在使用该程序的时候不要删除版权信息，Thanks♪(･ω･)ﾉ！
-$host = "mc.bili33.top"; //设置服务器域名或 IP
-$port = "28564"; //设置服务器端口，可以直接用服务器的链接端口，也可以使用query端口（UDP协议）
+$host = "mc.example.com"; //设置服务器域名或 IP
+$port = "25565"; //设置服务器端口，可以直接用服务器的链接端口，也可以使用query端口（UDP协议）
+$name = 'ExampleName'; // 服务器名字，会显示在标签页的标题和网站打头的大标题处
+$iconlink = 'https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@1.0/AboutMe/logo-mini.png'; // 网站图标链接，可以使图片格式（.png/.jpg/etc.）或者是ico图标，用绝对路径或相对路径均可
+$description = 'Server Status by <a href="https://bili33.top">GamerNoTitle</a> | Tamplate by <a href="https://html5up.net">HTML5UP</a><br>Open Source Project <a href="https://github.com/GamerNoTitle/Minecraft-Server-Status">Minecraft-Server-Status</a> is now avaliable on <a href="https://github.com/GamerNoTitle/Minecraft-Server-Status">Github</a>'; // 描述，会显示在大标题的下方，用html语法写就可以了，换行请用<br>
+$imagelink = 'https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@1.0/AboutMe/logo-mini.png' // 图片链接，会显示在服务器信息右边的圈圈里
 /*
 $host = $_GET["host"];
 $port = $_GET["port"];
@@ -12,8 +16,8 @@ require_once __DIR__.'/data.php';
 ?>
 <html>
 	<head>
-		<title>EMUnion</title>
-		<link rel="shortcut icon" href="https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@1.0/AboutMe/logo-mini.png">
+		<title><?php echo $name; ?></title>
+		<link rel="shortcut icon" href=<?php echo $iconlink; ?>>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
@@ -27,10 +31,8 @@ require_once __DIR__.'/data.php';
 				<!-- Header -->
 					<header id="header" class="alt">
 						<span class="logo"><img src="images/logo.svg" alt="" /></span>
-						<h1>EMUnion</h1>
-						<p>EMUnion Hosted by <a href="https://bili33.top">GamerNoTitle</a><br />
-						Server Status by <a href="https://bili33.top">GamerNoTitle</a> | Tamplate by <a href="https://html5up.net">HTML5UP</a><br />
-						Open Source Project <a href="https://github.com/GamerNoTitle/Minecraft-Server-Status">Minecraft-Server-Status</a> is now avaliable on <a href="https://github.com/GamerNoTitle/Minecraft-Server-Status">Github</a></p>
+						<h1><?php echo $name; ?></h1>
+						<p><?php echo $description; ?></p>
 					</header>
 
 				<!-- Main -->
@@ -78,7 +80,7 @@ require_once __DIR__.'/data.php';
 											<li><a href="https://html5up.net" class="button">HTML5UP</a></li>
 										</ul>
 									</div>
-									<span class="image"><img src="https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@1.0/AboutMe/logo-mini.png" alt="" /></span>
+									<span class="image"><img src=<?php echo $imagelink; ?> alt="" /></span>
 								</div>
 							</section>
 			</div>
