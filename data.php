ebb9e892adf6cf68f7c98ad1e85cf82de41cc91b
GamerNoTitle <GamerNoTitle@outlook.com>
2023-01-04 11:32:39 +0800

    more condition

--- data.php
+++ data.php
@@ -113,8 +113,30 @@ if (($Info = $Query->GetInfo()) !== false) { //判断 Query 是否查询的到
     $hostNameHtml = str_replace("§d", '<font color="#FF55FF">', $hostNameHtml);
     $hostNameHtml = str_replace("§e", '<font color="#FFFF55">', $hostNameHtml);
     $hostNameHtml = str_replace("§f", '<font color="#FFFFFF">', $hostNameHtml);
-
-    $cleanHostName = str_replace(array("§k", "§l", "§m", "§n", "§o", "§r", "§0", "§1", "§2", "§3", "§4", "§5", "§6", "§7", "§8", "§9", "§a", "§b", "§c", "§d", "§e", "§f"), "", $InfoPing['description']); //清除服务器 MOTD 颜色参数
+    $hostNameHtml = str_replace("�k", "", $InfoPing['description']);
+    $hostNameHtml = str_replace("�l", "", $hostNameHtml);
+    $hostNameHtml = str_replace("�m", "", $hostNameHtml);
+    $hostNameHtml = str_replace("�n", "", $hostNameHtml);
+    $hostNameHtml = str_replace("�o", "", $hostNameHtml);
+    $hostNameHtml = str_replace("�r", '<font color="#">', $hostNameHtml);
+    $hostNameHtml = str_replace("�0", '<font color="#000000">', $hostNameHtml);
+    $hostNameHtml = str_replace("�1", '<font color="#0000AA">', $hostNameHtml);
+    $hostNameHtml = str_replace("�2", '<font color="#00AA00">', $hostNameHtml);
+    $hostNameHtml = str_replace("�3", '<font color="#00AAAA">', $hostNameHtml);
+    $hostNameHtml = str_replace("�4", '<font color="#AA0000">', $hostNameHtml);
+    $hostNameHtml = str_replace("�5", '<font color="#AA00AA">', $hostNameHtml);
+    $hostNameHtml = str_replace("�6", '<font color="#FFAA00">', $hostNameHtml);
+    $hostNameHtml = str_replace("�7", '<font color="#AAAAAA">', $hostNameHtml);
+    $hostNameHtml = str_replace("�8", '<font color="#555555">', $hostNameHtml);
+    $hostNameHtml = str_replace("�9", '<font color="#5555FF">', $hostNameHtml);
+    $hostNameHtml = str_replace("�a", '<font color="#55FF55">', $hostNameHtml);
+    $hostNameHtml = str_replace("�b", '<font color="#55FFFF">', $hostNameHtml);
+    $hostNameHtml = str_replace("�c", '<font color="#FF5555">', $hostNameHtml);
+    $hostNameHtml = str_replace("�d", '<font color="#FF55FF">', $hostNameHtml);
+    $hostNameHtml = str_replace("�e", '<font color="#FFFF55">', $hostNameHtml);
+    $hostNameHtml = str_replace("�f", '<font color="#FFFFFF">', $hostNameHtml);
+    
+    $cleanHostName = str_replace(array("§k", "§l", "§m", "§n", "§o", "§r", "§0", "§1", "§2", "§3", "§4", "§5", "§6", "§7", "§8", "§9", "§a", "§b", "§c", "§d", "§e", "§f", "�k", "�l", "�m", "�n", "�o", "�r", "�0", "�1", "�2", "�3", "�4", "�5", "�6", "�7", "�8", "�9", "�a", "�b", "�c", "�d", "�e", "�f"), "", $InfoPing['description']); //清除服务器 MOTD 颜色参数
 
     $status = "在线"; //服务器状态
     $platform = "使用 Ping 查询无法取得数据"; //服务器平台 (MINECRAFT or MINECRAFTPE)
